622fe9ef3e3bc61438b3b0b835ba21496bb530ce
imnotmoon <imnotmoon@gmail.com>
2021-03-23 13:37:05 +0900

    startup name generator 1부

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -9,26 +9,53 @@ class MyApp extends StatelessWidget {
     final wordPair = WordPair.random();
 
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          // child: Text('Hello World'),
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 }
 
 // extend state : RamdonWords 클래스의 state를 보관
 class RandomWordsState extends State<RandomWords> {
+  // 식별자 앞에 밑줄을 붙이면 private이다.
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd)
+          return Divider(
+            thickness: 1,
+          );
+
+        final index = i ~/ 2; // python의 //연산자와 같음
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StartUp Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
 
