62c815b00c1532bc9372da3574aeb215518a8ac6
imnotmoon <imnotmoon@gmail.com>
2021-03-28 21:46:41 +0900

    ListView : Widgett을 리턴하는 itemBuilder 함수를 통해 리스트 아이템 생성

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -27,9 +27,34 @@ class RandomWords extends StatefulWidget {
 }
 
 class _RandomWordsState extends State<RandomWords> {
+  List<WordPair> words = [];
+
   @override
   Widget build(BuildContext context) {
-    final wordpair = WordPair.random();
-    return Text(wordpair.asCamelCase);
+    return ListView.builder(
+      // 리스트 원소가 하나씩 만들어질때마다 itemBuilder 함수가 호출.
+      itemBuilder: (BuildContext context, int index) {
+        if (index >= words.length) {
+          // 인덱스가 같으면 단어를 10개 추가
+          // 다음 listItem부터 10번째 item까지는 인덱스가 더 작기 때문에 여기 안들어옴
+          words.addAll(generateWordPairs().take(10));
+        }
+        // 아래 위젯을 생성.
+        // print(words[index]);
+        return _getRow(words[index]);
+      },
+    );
+  }
+
+  Widget _getRow(WordPair wordPair) {
+    // 여기서 wordPair는 word[index].
+    // 단어 하나
+    return Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          wordPair.asCamelCase,
+          textScaleFactor: 1.8,
+          // style: TextStyle(fontSize: 10),
+        ));
   }
 }
